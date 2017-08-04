a01ad9232b0175a9dc274d62dd90d1994a7c9c76
uojo <499620917@qq.com>
2017-08-04 19:53:04 +0800

    save

--- test/lodash.js
+++ test/lodash.js
@@ -1,7 +1,20 @@
 const {clog} = require('../app/utils')
 const _ = require("lodash");
+let da1 = {
+	a:{
+		b:{
+			c:3,
+			// d:4,
+			e:5
+		}
+	}
+};
+let {c,d=2} = da1.a.b
+clog(c,d)
 
-clog( _.isEmpty(false) )
+clog( _.omit(da1,['a.b.d'])  )
 clog( _.has(false,'a') )
 clog( _.has({a:0},'a') )
+clog( _.isEmpty(false), _.isEmpty([]), _.isEmpty({}) )
 
+clog( __dirname )
