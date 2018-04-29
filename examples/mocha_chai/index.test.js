7a29d77497b7a47e81e8ccb23240c3c7d029c6ee
uojo <friend1020@163.com>
2018-04-29 23:44:49 +0800

    feature jsdom ajax

--- examples/mocha_chai/index.test.js
+++ examples/mocha_chai/index.test.js
@@ -2,13 +2,15 @@
 // const {assert} = require('chai')
 var fn3 = require('./cjs/mod1');
 var {add} = require('ramda')
+var {ajax} = require('jquery')
 // console.log(add)
 // console.log(assert)
 // var assert = chai.assert;
 // console.log(1,fn3)
+// console.log('jquery.ajax',ajax);
 
 describe('chai.assert',function(){
-  console.log('describe.run', document);
+  // console.log('describe.run', document);
 
   it('deepEqual',function(){
     assert.deepEqual({ tea: 'green', b:{a:1} }, { tea: 'green', b:{a:1} });
@@ -20,6 +22,17 @@ describe('chai.assert',function(){
     assert.include({ foo: 'bar', hello: 'universe' }, { foo: 'bar' }, 'object contains property');
     assert.include({ foo: 'bar', hello: 'universe' }, {}, 'object contains property');
     
+  })
+
+  it('async.jquery.ajax',function(done){
+    ajax({
+      url:'http://localhost:3000/data/_true',
+      success:function(e){
+        console.log('ajax.success',e);
+        assert(true,'hello')
+        done()
+      }
+    });
   })
 
 })
