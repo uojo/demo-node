99032403900ffd8b41308cde55b86b7fd8b8d989
uojo <friend1020@163.com>
2019-05-30 11:54:21 +0800

    fix(pageres): 用例

--- examples/pageres/b.js
+++ examples/pageres/b.js
@@ -12,7 +12,7 @@ const pageres = new Pageres(options)
 // 视频失败 #figure_video
 // .src('www.qq.com', ['145x80'], {crop: true})
 .src('127.0.0.1:7001/screenshot', ['400x400'], {crop: false})
-// .src('www.dxy.cn', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
+// .src('www.baidu.com', ['480x320', '1024x768', 'iphone 5s'], {crop: true})
 // .src('www.163.com', ['1280x1024', '1920x1080'])
 // .src('data:text/html;base64,PGgxPkZPTzwvaDE+', ['1024x768'])
 .dest( path.resolve(__dirname,'./dist') )
