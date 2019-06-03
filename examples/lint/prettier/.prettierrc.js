5e8c1fd1e1a413d04f86672d9f5fc2e84bcf53ec
uojo <friend1020@163.com>
2019-06-03 16:31:46 +0800

    style(lint): prettier 配置文件注释

--- examples/lint/prettier/.prettierrc.js
+++ examples/lint/prettier/.prettierrc.js
@@ -8,12 +8,16 @@ module.exports = {
   "singleQuote": false,
   "quoteProps": "as-needed",
   "jsxSingleQuote": false,
+  // 逗号跟随
   "trailingComma": "none",
+  // 大括号内的空格
   "bracketSpacing": true,
   "jsxBracketSameLine": false,
+  // 箭头函数的参数括号，当需要时
   "arrowParens": "avoid",
   "rangeStart": 0,
   "rangeEnd": Infinity,
+  // 解析器，可选值：https://prettier.io/docs/en/options.html#parser
   "parser": "babel",
   "requirePragma": false,
   "proseWrap": "preserve",
