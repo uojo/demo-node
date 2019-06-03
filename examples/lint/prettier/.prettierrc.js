051b8d1b64745c338898d2071f66050123d5210d
uojo <friend1020@163.com>
2019-06-03 15:08:07 +0800

    feat(lint): prettier

---
+++ examples/lint/prettier/.prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  "printWidth": 80,
+  "tabWidth": 2,
+  // 使用tab替换空格
+  "tab": false,
+  // 分号
+  "semi": true,
+  "singleQuote": false,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": false,
+  "trailingComma": "none",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": false,
+  "arrowParens": "avoid",
+  "rangeStart": 0,
+  "rangeEnd": Infinity,
+  "parser": "none",
+  "requirePragma": false,
+  "proseWrap": "preserve",
+  "htmlWhitespaceSensitivity": "css",
+  "endOfLine": "auto"
+}
